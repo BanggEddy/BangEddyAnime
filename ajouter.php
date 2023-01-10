249c812e00f719dda7b60f63087deedf83751952
unknown <davidli4532@gmail.com>
2023-01-10 19:39:28 +0100

    update site

--- ajouter.php
+++ ajouter.php
@@ -1,70 +1,70 @@
 <?php include("headeradmin.php") ?>
 
+<br>
 
-
-    <div class="container">
-      <form method="POST" action="ajouter_form.php" enctype="multipart/form-data">
-        <div class="row">
-          <div class="col-25">
-            <label for="Prénom">Nom de l'anime</label>
-          </div>
-          <div class="col-75">
-            <input type="text" name="nom" placeholder="Nom de l'anime">
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-25">
-            <label for="Prix">la note</label>
-          </div>
-          <div class="col-75">
-            <input type="text" name="note" placeholder="Saisir la note (A-E)">
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-25">
-            <label for="Prix">Nombre d'épisodes </label>
-          </div>
-          <div class="col-75">
-            <input type="text" name="eprn" placeholder="Sasir le nombre d'épisode actuallement">
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-25">
-            <label for="Prix">L'anime est-il finis ou pas </label>
-          </div>
-          <div class="col-75">
-            <input type="text" name="yn" placeholder="Saisir O ou N">
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-25">
-            <label for="Prix">le lien du site pour visionner </label>
-          </div>
-          <div class="col-75">
-            <input type="text" name="lien" placeholder="Saisir le lien du site pour visualiser">
-          </div>
-        </div>
-
-
-
-        <div class="row">
-          <div class="col-25">
-            <label for="Sujet">Images</label>
-          </div>
-          <div class="col-75">
-            <input type="file" name="photo" placeholder="Selectionnez l'image" ></textarea>
-          </div>
-        </div>
-
-        <div class="row">
-          <input type="submit" value="Envoyer">
-        </div>
-      </form>
+<div class="container">
+  <form method="POST" action="ajouter_form.php" enctype="multipart/form-data">
+    <div class="row">
+      <br><br>
+      <div class="col-25">
+        <label for="Anime name" style="color:#f5b7f5 ;">Nom de l'anime</label>
+      </div>
+      <div class="col-75">
+        <input type="text" name="nom" placeholder="Nom de l'anime">
+      </div>
     </div>
 
-    <?php include("footer.php") ?>
+    <div class="row">
+      <div class="col-25">
+        <label for="Prix" style="color:#f5b7f5 ;">Note</label>
+      </div>
+      <div class="col-75">
+        <input type="text" name="note" placeholder="Saisir la note (A-E)">
+      </div>
+    </div>
 
+    <div class="row">
+      <div class="col-25">
+        <label for="Prix" style="color:#f5b7f5 ;">Nombre d'épisodes </label>
+      </div>
+      <div class="col-75">
+        <input type="text" name="eprn" placeholder="Sasir le nombre d'épisode actuallement">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-25">
+        <label for="Prix" style="color:#f5b7f5 ;">L'anime est-il finis ou pas </label>
+      </div>
+      <div class="col-75">
+        <input type="text" name="yn" placeholder="Saisir O ou N">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-25">
+        <label for="Prix" style="color:#f5b7f5 ;">le lien du site pour visionner </label>
+      </div>
+      <div class="col-75">
+        <input type="text" name="lien" placeholder="Saisir le lien du site pour visualiser">
+      </div>
+    </div>
+
+
+
+    <div class="row">
+      <div class="col-25">
+        <label for="Sujet" style="color:#f5b7f5 ;">Images</label>
+      </div>
+      <div class="col-75">
+        <input type="file" name="photo" placeholder="Selectionnez l'image"></textarea>
+      </div>
+    </div>
+    <br><br>
+    <div class="row">
+      <input type="submit" value="Envoyer">
+    </div>
+  </form>
+</div>
+
+<?php include("footer.php") ?>
--- header.php
+++ header.php
@@ -1,67 +1,45 @@
 <?php
-session_start() ;
+session_start();
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  </head>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
 
 
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="index.php"><img src="Images/logo.jpg" height="100" width="100" alt="..."></a>
-        <div class="btn-group">
-          <a href="scan.php" class="btn btn-secondary" aria-current="page">Page de Scan</a>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php"><img src="Images/logo.jpg" height="100" width="100" alt="..."></a>
+      <div class="btn-group">
+        <a href="scan.php" class="btn btn-secondary" aria-current="page">Page de Scan</a>
+      </div>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+        <div class="container-fluid">
+          <form class="d-flex" method="POST" action='recherchevisi.php'>
+            <input class="form-control me-2" type="search" placeholder="Rechercher des bonbons" aria-label="Recherche" name="recherche">
+            <input class="btn btn-outline-success" type="submit" value="Rechercher">
+          </form>
         </div>
-        
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-          <div class="container-fluid">
-            <form class="d-flex" method="POST" action='recherchevisi.php'>
-              <input class="form-control me-2" type="search" placeholder="Rechercher des bonbons" aria-label="Recherche" name="recherche">
-              <input class="btn btn-outline-success" type="submit" value="Rechercher" >
-            </form>
-          </div>
-        </div>
-        <div class="btn-group">
+      </div>
+      <div class="btn-group">
         <a href="indexa.php" class="btn btn-outline-secondary" aria-current="page">A</a>
-          <a href="indexb.php" class="btn btn-outline-secondary" aria-current="page">B</a>
-          <a href="indexc.php" class="btn btn-outline-secondary" aria-current="page">C</a>
-          <a href="indexd.php" class="btn btn-outline-secondary" aria-current="page">D</a>
-          <a href="indexe.php" class="btn btn-outline-secondary" aria-current="page">E</a>
-          </div>
+        <a href="indexb.php" class="btn btn-outline-secondary" aria-current="page">B</a>
+        <a href="indexc.php" class="btn btn-outline-secondary" aria-current="page">C</a>
+        <a href="indexd.php" class="btn btn-outline-secondary" aria-current="page">D</a>
+        <a href="indexe.php" class="btn btn-outline-secondary" aria-current="page">E</a>
       </div>
-      <!-- Button trigger modal -->
-<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
-Administration
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Administration</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form method="POST" action="admin.php">
-
-        <label> Identifiant : </label> <input type="text" placeholder="Votre  Identifiant" name="id">
-        <br>
-        <label> Mot De Passe : </label> <input type="password" placeholder="Votre  Mot De Passe" name="mdp">
-        <br>
-        <input type="submit" value="Valider" class='btn btn-danger'>
-        </form>
-        </div>
     </div>
-  </div>
-</div>
-    </nav>
+    <!-- Button trigger modal -->
+    <a href="indexadmin.php" type="button" class="btn btn-danger">
+      Administration
+    </a>
+
+  </nav>
